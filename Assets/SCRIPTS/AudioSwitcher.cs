416f978c1e4f2f4db6726824067c94c93c5dc9a1
JMMW <joe@mottandwhite.com>
2020-04-20 16:14:40 +0100

    Full Project First Commit

---
+++ Assets/SCRIPTS/AudioSwitcher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioSwitcher : MonoBehaviour
+{
+    public AudioMixerSnapshot stereo;
+    public AudioMixerSnapshot spatialised;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(OVRInput.Get(OVRInput.Button.One))
+        {
+            stereo.TransitionTo(.01f);
+        }
+
+        if(OVRInput.Get(OVRInput.Button.Two))
+        {
+            spatialised.TransitionTo(.01f);
+        }
+    }
+}
---
+++ Assets/SCRIPTS/PanoramaCapture.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanoramaCapture : MonoBehaviour
+{
+    public Camera TargetCamera;
+    public RenderTexture CubeMapLeft;
+    public RenderTexture equirectRT;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Capture();
+        }
+    }
+    void Capture()
+    {
+        TargetCamera.RenderToCubemap(CubeMapLeft);
+        CubeMapLeft.ConvertToEquirect(equirectRT);
+    }
+    public void Save(RenderTexture rt)
+    {
+        Texture2D tex = new Texture2D(rt.width, rt.height);
+
+        RenderTexture.active = rt;
+        tex.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+        RenderTexture.active = null;
+
+        byte[] bytes = tex.EncodeToJPG();
+
+        string path = Application.dataPath + "/Panorama" + ".jpg";
+
+        System.IO.File.WriteAllBytes(path, bytes);
+    }
+}
